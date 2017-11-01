39fefd1fc35e5bfe41685b4e0e767e039635bb21
prybytkov <i.prybytkov@gmail.com>
2017-11-01 02:21:06 +0200

    L3-4

---
+++ HomeWork/L3/4-functions-simple.js
@@ -0,0 +1,95 @@
+// 1 task
+// Create function "getName" that ask user his name with promt window and alerts his name to him in format: "Hello, <his_name>".
+
+
+    function getName(name) {
+    var name = prompt('Name');
+           alert('Hello, ' + name)
+}
+
+getName(); // "Hello, Lily";
+
+
+// 2 task
+
+// Create function double(x) that returns the number passed to it as an argument, multiply with 2
+
+    function double(x) {
+        return x * 2
+           }
+
+
+double(6); // logs in console => 12
+
+
+// 3 task
+// Create function range(x, y) that returns a sum of digits in the range of numbers [x, y];
+
+function range(x, y) {
+
+    var res = 0;
+
+   if (x < y) {
+       for( var a = x; a <= y; a++) {
+       res += a;
+             }
+    }
+            else if  (x > y) {
+                for (var b = y; b => x ; b++) {
+               res += b;
+                    }
+            }
+                else if ( x = y) {
+                     res += x;
+                     }
+   return res;
+}
+
+range(1,3); // logs in console => 6
+
+
+
+// 4 task
+// Напишите функцию которая будет находить сумму простых чисел меньших переданному в нее значению. Пример работы:
+
+    // function getPrimesSumBelow(x) {
+
+        // var res = 0;
+
+//            for( var a = x; a/; a++) {
+//                 res += a;
+//             }
+
+    // }
+
+// getPrimesSumBelow(10); // 2 + 3 + 5 + 7 = 17
+ БЕДА=)
+
+
+// 5 task
+// Create a function that returns true if number is odd, and false if even;
+
+function odd(x) {
+        return (x % 2 !== 0);
+}
+
+// 6 task
+// Create a function that returns all odd numbers in range, use the previous function inside this one;
+БЕДА=)№2
+
+// 7 task
+// Create a function that will call promt window that will ask for a number to user, until he enters 9;
+
+function num(x) {
+
+    var x = +prompt('Number');
+
+    if (x !==9){
+       num ()
+    }           else {
+        alert("NINE!")
+    }
+
+}
+
+num ()
